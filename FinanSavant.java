90c2b362a9f7ad8b84d0159634a7491a265971dc
DanielIm908 <imcha03@ecolecatholique.ca>
2026-05-19 02:07:29 -0400

    Update FinanSavant.java
    Added comments to clarify the main class functionality and execution context.

--- FinanSavant.java
+++ FinanSavant.java
@@ -1,8 +1,11 @@
 import javax.swing.*;
 
-/** Point d'entrée de l'application FinanSavant. */
+// Classe principale qui démarre le programme
 public class FinanSavant {
   public static void main(String[] args) {
+
+    // Exécute la fenêtre dans le thread graphique de Swing (EDT) afin d’éviter des problèmes d’affichage et de synchronisation.
+    // Source : https://www.reddit.com/r/learnprogramming/comments/29ik8n/java_can_someone_explain/
     SwingUtilities.invokeLater(() -> new FenetrePrincipale());
   }
 }
