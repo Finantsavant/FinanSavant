9e586b96e1baa8fdc4534a1861e007e78d4a77fc
DanielIm908 <imcha03@ecolecatholique.ca>
2026-05-19 02:04:09 -0400

    Update DonneesUtilisateur.java
    Added comments to clarify the purpose of the class and its constructor.

--- DonneesUtilisateur.java
+++ DonneesUtilisateur.java
@@ -1,14 +1,18 @@
 import java.util.ArrayList;
 
-/** Informations personnelles et objectifs d'un utilisateur. */
+// Classe qui stocke toutes les informations liées à un utilisateur.
+// Ces informations seront utilisées dans not outils durant les proces de calcul.
 class DonneesUtilisateur {
   String nomAffichage;
   int age;
+  // Pourcentage d'argent voulu pour l'outil d'investissement
   int pourcentInvestissement;
   String occupation;
   ArrayList<Objectif> objectifs = new ArrayList<>();
 
+  // Constructeur
   public DonneesUtilisateur(String nomAffichage, int age, int pourcentInvestissement, String occupation) {
+    // Initialisation des informations
     this.nomAffichage = nomAffichage;
     this.age = age;
     this.pourcentInvestissement = pourcentInvestissement;
